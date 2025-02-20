9bce92b5c8815cfab82544117ad6247fac36c8f6
binyongbin <kyb2475@gmail.com>
2025-02-21 03:24:15 +0900

    fix: navbar 스타일 버그 수정

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,49 +1,43 @@
 import Link from "next/link";
-import homeIcon from "@/assets/icons/homeIcon.svg";
-import listIcon from "@/assets/icons/listIcon.svg";
-import tableIcon from "@/assets/icons/tableIcon.svg";
+import analysisIcon from "@/assets/icons/analysisIcon.svg";
+import sell from "@/assets/icons/sell.svg";
+import checklist from "@/assets/icons/checklist.svg";
 import Image from "next/image";
 
 const navItems = [
-    { href: "/", label: "성장판" },
-    { href: "/space", label: "공간" },
-    { href: "/administration", label: "관리" },
+  { href: "/", label: "성장판" },
+  { href: "/space", label: "공간" },
+  { href: "/administration", label: "관리" },
 ];
 
 function NavItem({ href, label }: { href: string; label: string }) {
-    return (
-        <Link href={href} className="flex flex-col items-center gap-[10px]">
-            {label === "성장판" && (
-                <Image src={homeIcon} alt="homeIcon" width={24} height={24} />
-            )}
+  return (
+    <Link href={href} className="flex flex-col items-center ">
+      {label === "성장판" && (
+        <Image src={analysisIcon} alt="homeIcon" width={24} height={24} />
+      )}
+      {label === "공간" && (
+        <Image src={sell} alt="tableIcon" width={24} height={24} />
+      )}
+      {label === "관리" && (
+        <Image src={checklist} alt="listIcon" width={24} height={24} />
+      )}
 
-            {label === "공간" && (
-                <Image src={tableIcon} alt="tableIcon" width={24} height={24} />
-            )}
-
-            {label === "관리" && (
-                <Image src={listIcon} alt="listIcon" width={24} height={24} />
-            )}
-
-            <span className="text-[12px] leading-24 tracking-[-0.1] h-24 flex items-center font-medium">
-                {label}
-            </span>
-        </Link>
-    );
+      <span className="text-[12px] leading-24 tracking-[-0.1] h-24 flex items-center font-medium">
+        {label}
+      </span>
+    </Link>
+  );
 }
 
 export default function Navbar(): JSX.Element {
-    return (
-        <div className="w-390 h-94 sticky bottom-0 flex justify-center items-center bg-white bg-opacity-90 backdrop-blur-xl">
-            <div className="flex gap-[82px]">
-                {navItems.map((item) => (
-                    <NavItem
-                        key={item.href}
-                        href={item.href}
-                        label={item.label}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-390 h-94 sticky bottom-0 flex justify-center items-center bg-white bg-opacity-90 backdrop-blur-xl">
+      <div className="flex gap-x-82 items-center justify-center">
+        {navItems.map((item) => (
+          <NavItem key={item.href} href={item.href} label={item.label} />
+        ))}
+      </div>
+    </div>
+  );
 }
