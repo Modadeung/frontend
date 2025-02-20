6f91b18805d0a3da480f890b9c3169b7069a7d83
99andrew99 <whalstjr199927@gmail.com>
2025-02-21 04:39:10 +0900

    [feat] 공간 상세 api 연결

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,21 @@ const nextConfig = {
     images: {
         domains: ["i.ibb.co"], // 외부 이미지 도메인 허용
     },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.mp4$/,
+            use: {
+                loader: "file-loader",
+                options: {
+                    publicPath: "/_next/static/videos/",
+                    outputPath: "static/videos/",
+                    name: "[name].[hash].[ext]",
+                },
+            },
+        });
+
+        return config;
+    },
 };
 
 export default nextConfig;
