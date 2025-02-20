cc0c486d85e06a2c136e0d2b8bcdff448b189189
binyongbin <kyb2475@gmail.com>
2025-02-20 16:41:24 +0900

    chore: 프리티어 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,53 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
-// const colorPalette: Record<
-//   string,
-//   string | Record<string | number, string | Record<string, string>>
-// > = {
-//   // 나오면 정의
-// }
+const px0_20 = Array.from(Array(21)).reduce((acc, _, i) => {
+  acc[i] = `${i}px`;
+  return acc;
+}, {} as Record<number, string>);
 
-// const fontPalette: Record<
-//   string,
-//   [string, { lineHeight: string; letterSpacing: string }] | string
-// > = {
-// 	// 나오면 정의
-// }
+const px0_100 = Array.from(Array(101)).reduce((acc, _, i) => {
+  acc[i] = `${i}px`;
+  return acc;
+}, {} as Record<number, string>);
 
-const px0_20 = Array.from(Array(21)).reduce(
-  (acc, _, i) => {
-    acc[i] = `${i}px`
-    return acc
-  },
-  {} as Record<number, string>
-)
-
-const px0_100 = Array.from(Array(101)).reduce(
-  (acc, _, i) => {
-    acc[i] = `${i}px`
-    return acc
-  },
-  {} as Record<number, string>
-)
-
-const px0_1200 = Array.from(Array(1201)).reduce(
-  (acc, _, i) => {
-    acc[i] = `${i}px`
-    return acc
-  },
-  {} as Record<number, string>
-)
+const px0_1200 = Array.from(Array(1201)).reduce((acc, _, i) => {
+  acc[i] = `${i}px`;
+  return acc;
+}, {} as Record<number, string>);
 
 const config: Config = {
-  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  content: ["src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       borderRadius: px0_100,
     },
     screens: {
-      sm: { min: '390px', max: '767px' },
-      md: { min: '768px', max: '1199px' },
-      lg: { min: '1200px' },
+      sm: { min: "390px", max: "767px" },
+      md: { min: "768px", max: "1199px" },
+      lg: { min: "1200px" },
     },
     borderWidth: px0_20,
     minWidth: px0_1200,
@@ -55,5 +32,5 @@ const config: Config = {
     spacing: px0_1200,
   },
   plugins: [],
-}
-export default config
+};
+export default config;
