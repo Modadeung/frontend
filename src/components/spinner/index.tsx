747fb19a738091c501b6bcfaeddea88629cf323e
Yongbin Ko <kyb2475@gmail.com>
2025-02-20 17:04:32 +0900

    refactor: 스피너 컴포넌트 변경 (react-spinner 적용) (#14)
    * chore: react-spinner 라이브러리 설치
    
    * refactor: 스피너에 react-spinner 저굥ㅇ

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,13 +1,14 @@
+import RiseLoader from "react-spinners/RiseLoader";
+
 interface SpinnerProps {
   className?: string;
 }
 
 export default function Spinner({ className }: SpinnerProps): JSX.Element {
   return (
-    <div className={className}>
-      {/* TODO: gif 또는 video로 적용 */}
-      <div className="w-146 h-146 bg-black"></div>
-      <span className="text-center pt-36 text-[16px] font-medium leading-24">
+    <div className={`border-1 ${className}`}>
+      <RiseLoader color="#ff9d00" />
+      <span className="text-center pt-50 text-[16px] font-medium leading-24">
         정보를 바탕으로 <br />
         최적의 협업 파트너를 <br />
         찾고 있어요!
