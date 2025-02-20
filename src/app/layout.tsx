e7871c278c82f21c0b5e49daac6207ffffbf9969
Yongbin Ko <kyb2475@gmail.com>
2025-02-20 13:36:20 +0900

    feat: 역할이 소상공인일 경우의 카테고리 선택 페이지 구현 (#6)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,21 @@
 import "@/styles/globals.css";
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="ko">
-            <head>
-                <link
-                    rel="stylesheet"
-                    as="style"
-                    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
-                    crossOrigin="anonymous"
-                />
-            </head>
-            <body className="w-390 max-h-844 m-auto overflow-y-scroll">
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body className="w-390 h-844 m-auto overflow-y-scroll">{children}</body>
+    </html>
+  );
 }
